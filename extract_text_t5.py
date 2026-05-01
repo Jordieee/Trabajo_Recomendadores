8d20876b0cab879db457e0ef298b1f2cd7fdc319
jlincar <jlincar@epsa.upv.es>
2026-05-01 17:38:44 +0200

    Repo limpio

--- extract_text_t5.py
+++
@@ -1,9 +0,0 @@
-import fitz
-doc = fitz.open('Trabajo 5. SR Híbrido.pdf')
-text = ""
-for page in doc:
-    text += page.get_text()
-    
-with open('t5_reqs.txt', 'w', encoding='utf-8') as f:
-    f.write(text)
-print("Text extracted to t5_reqs.txt")
